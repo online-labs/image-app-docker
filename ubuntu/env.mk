ebc82efdb542fe682710ced5670508e9aa03c8e7
Jérôme Tamba <jtamba@online.net>
2017-11-22 15:43:03 +0100

    Convert image to the new build system

---
+++ ubuntu/env.mk
@@ -0,0 +1,8 @@
+IMAGE_NAME = docker
+IMAGE_VERSION = ubuntu
+IMAGE_VERSION_ALIASES =
+IMAGE_TITLE = Docker
+IMAGE_DESCRIPTION = Docker + Docker-Compose + gosu + pipework
+IMAGE_SOURCE_URL = https://github.com/scaleway-community/scaleway-docker
+IMAGE_BOOTSCRIPT = mainline 4.4
+IMAGE_NAME = Docker
